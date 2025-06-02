1ce78bdf0ea7c6fd6e7e0131a84e91f858fabf32
ANIRBAN DEY <62869656+AnirbanDey-9497@users.noreply.github.com>
2025-06-02 11:32:21 -0400

    Trees

---
+++ src/Trees/Traversal.java
@@ -0,0 +1,12 @@
+package src.Trees;
+
+public class Traversal {
+
+    public static void main(String[] args)
+    {
+        Node newNode= new  Node(5);
+        newNode.left= new Node(2);
+        newNode.right=new Node(4);
+        newNode.right.left=new Node(9);
+    }
+}
